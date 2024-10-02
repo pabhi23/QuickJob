511e313fca9099c73e386b72ac879a0ab655bcc2
arundinesh1011@gmail.com <arundinesh1011@gmail.com>
2024-10-02 00:58:40 -0400

    Add form fields for first name, last name, contact number, and email

--- src/Account/AccountPage.js
+++ src/Account/AccountPage.js
@@ -14,6 +14,51 @@ const AccountPage = () => {
         <div className="profile-picture">
           <img src="default-profile.png" alt="Profile" /> {}
         </div>
+        <form className="account-form">
+  <div className="form-group">
+    <label htmlFor="firstName">First Name</label>
+    <input
+      type="text"
+      id="firstName"
+      value={firstName}
+      onChange={(e) => setFirstName(e.target.value)} 
+      required
+    />
+  </div>
+
+  <div className="form-group">
+    <label htmlFor="lastName">Last Name</label>
+    <input
+      type="text"
+      id="lastName"
+      value={lastName}
+      onChange={(e) => setLastName(e.target.value)}
+      required
+    />
+  </div>
+
+  <div className="form-group">
+    <label htmlFor="contactNumber">Contact Number</label>
+    <input
+      type="tel"
+      id="contactNumber"
+      value={contactNumber}
+      onChange={(e) => setContactNumber(e.target.value)} 
+      required
+    />
+  </div>
+
+  <div className="form-group">
+    <label htmlFor="email">Email</label>
+    <input
+      type="email"
+      id="email"
+      value={email}
+      onChange={(e) => setEmail(e.target.value)} 
+      required
+    />
+  </div>
+</form>
       </div>
     </div>
   );
