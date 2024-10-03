0602d89d1113000f00ee7369afebc49ff6b02a84
Akshay Bhut <akshaybhut14@gmail.com>
2024-10-03 00:17:44 -0400

    Changes: Media Queries for Login, Register and Dashboard

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,40 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
 import { useNavigate, Link } from 'react-router-dom';
-import { FaMoon } from 'react-icons/fa'; // Dark mode icon
-import logo from '../Img/Logo.png'
+import { FaMoon, FaBars, FaTimes } from 'react-icons/fa'; // Dark mode and hamburger icons
+import logo from '../Img/Logo.png';
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false); // State to handle menu toggle
   const navigate = useNavigate();
-  const userName = "Abhi Patel"; 
-  const userPhoto = "https://via.placeholder.com/40"; 
+  const userName = "Abhi Patel";
+  const userPhoto = "https://via.placeholder.com/40";
 
   // Function to navigate to the home page
   const handleLogoClick = () => {
     navigate('/');
   };
 
+  // Function to toggle the menu
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <header className="header">
       <div className="navbar-container">
-      <img 
+        <img 
           src={logo} 
           alt="QuickJob Logo" 
           className="logo" 
           onClick={handleLogoClick} 
-          style={{ cursor: 'pointer' }}
         />
-        <nav className="navbar">
+        <nav className={`navbar ${menuOpen ? 'active' : ''}`}>
           <Link to="/register" className="nav-link">Register</Link>
           <Link to="/login" className="nav-link">Login</Link>
         </nav>
-        <div className="user-info">
-          <div className="dark-mode-toggle">
-            <FaMoon className="dark-mode-icon" />
+        <div className="user-menu">
+          <div className="user-info">
+            <div className="dark-mode-toggle">
+              <FaMoon className="dark-mode-icon" />
+            </div>
+            <div className="user-profile">
+              <img src={userPhoto} alt="User" className="user-photo" />
+              <span className="user-name">{userName}</span>
+            </div>
           </div>
-          <div className="user-profile">
-            <img src={userPhoto} alt="User" className="user-photo" />
-            <span className="user-name">{userName}</span>
+          <div className="menu-icon" onClick={toggleMenu}>
+            {menuOpen ? <FaTimes className="hamburger-icon" /> : <FaBars className="hamburger-icon" />}
           </div>
         </div>
       </div>
