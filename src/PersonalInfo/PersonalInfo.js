20499b1978bb9a16b2f9152737fb31efff4af343
Sasikenway <sasikenway@gmail.com>
2024-11-14 02:54:50 -0500

    updated resumebuilder

--- src/PersonalInfo/PersonalInfo.js
+++ src/PersonalInfo/PersonalInfo.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import '../PersonalInfo/PersonalInfo.css';
 
 const PersonalInfo = ({ nextStep, handleChange, formData }) => {
     const { firstName, lastName, address, jobTitle, linkedinId, phone, email, summary } = formData;
@@ -24,22 +25,39 @@ const PersonalInfo = ({ nextStep, handleChange, formData }) => {
     };
 
     return (
-        <div>
-            <h2>Personal Information</h2>
-            <input type="text" placeholder="First Name" value={firstName} onChange={(e) => handleChange('firstName', e.target.value)} />
-            <input type="text" placeholder="Last Name" value={lastName} onChange={(e) => handleChange('lastName', e.target.value)} />
-            <input type="text" placeholder="Address" value={address} onChange={(e) => handleChange('address', e.target.value)} />
-            <input type="text" placeholder="Desired Job Title" value={jobTitle} onChange={(e) => handleChange('jobTitle', e.target.value)} />
-            <input type="text" placeholder="LinkedIn ID" value={linkedinId} onChange={(e) => handleChange('linkedinId', e.target.value)} />
-            <input type="text" placeholder="Phone" value={phone} onChange={(e) => handleChange('phone', e.target.value)} />
-            <input type="text" placeholder="Email" value={email} onChange={(e) => handleChange('email', e.target.value)} />
-            <textarea 
-                placeholder="Summary" 
-                value={summary} 
-                onChange={(e) => handleChange('summary', e.target.value)}
-                rows="4"
-            />
-            <button onClick={handleNext}>Next</button>
+        <div className="form-wrapper">
+            <div className="form-container">
+                <h1 className='heading'>Build Your Resume</h1>
+                <h2 className='tag-line'>Create Professional Resumes Effortlessly with our customizable Resume Builder</h2>
+                <div className="input-row">
+                    <input className="personal-input" type="text" placeholder="First Name" value={firstName} onChange={(e) => handleChange('firstName', e.target.value)} />
+                    <input className="personal-input" type="text" placeholder="Last Name" value={lastName} onChange={(e) => handleChange('lastName', e.target.value)} />
+                </div>
+
+                <div className="input-row">
+                    <input className="personal-input" type="text" placeholder="Address" value={address} onChange={(e) => handleChange('address', e.target.value)} />
+                    <input className="personal-input" type="text" placeholder="Desired Job Title" value={jobTitle} onChange={(e) => handleChange('jobTitle', e.target.value)} />
+                </div>
+
+                <div className="input-row">
+                    <input className="personal-input" type="text" placeholder="LinkedIn ID" value={linkedinId} onChange={(e) => handleChange('linkedinId', e.target.value)} />
+                    <input className="personal-input" type="text" placeholder="Phone" value={phone} onChange={(e) => handleChange('phone', e.target.value)} />
+                </div>
+
+                <div className="input-row">
+                    <input className="personal-input" type="text" placeholder="Email" value={email} onChange={(e) => handleChange('email', e.target.value)} />
+                    <textarea
+                        className="personal-input"
+                        placeholder="Summary"
+                        value={summary}
+                        onChange={(e) => handleChange('summary', e.target.value)}
+                        rows="4"
+                    />
+                </div>
+                <div className="input-row">
+                    <button className="personal-input" onClick={handleNext}>Next</button>
+                </div>
+            </div>
         </div>
     );
 };
--- src/ResumeDetails/ResumeDetails.js
+++
@@ -1,4 +0,0 @@
-import React from "react";
-import Header from "../Header/Header";
-import '../ResumeDetails/ResumeDetails.css';
-
